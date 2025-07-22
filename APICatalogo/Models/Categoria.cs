d8be6301a273399cb9de7a6dab6f8e02234ba68c
tdias <dias.talitaa22@gmail.com>
2025-07-21 20:21:35 -0400

    apply data annotations

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace APICatalogo.Models;
 public class Categoria
 {
+    [Key]
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
     public ICollection<Produto> Produtos { get; set; } = new Collection<Produto>();
 }
