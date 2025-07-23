1ddf2ed898ee7d87dff488fbc62d9a10f821f070
tdias <dias.talitaa22@gmail.com>
2025-07-22 20:49:11 -0400

    feat:add ProdutosController and Get methods

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,32 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController(AppDbContext context) : ControllerBase
+    {
+        private readonly AppDbContext _context = context;
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+            if (produtos is null)
+                return NotFound("Produtos não encontrados!");
+            return produtos;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> GetById(int id)
+        {
+            var produto = _context.Produtos.SingleOrDefault(p => p.Id == id);
+            if (produto is null)
+                return NotFound($"Produto de Id {id} não encontrado!");
+            return produto;
+        }
+    }
+}
