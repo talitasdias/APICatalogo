86e0c51396c68bc6089213cf45474f3b254f91c5
tdias <dias.talitaa22@gmail.com>
2025-07-25 16:56:56 -0400

    feat:add CategoriasController and Get method

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,20 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController(AppDbContext context) : ControllerBase
+    {
+        private readonly AppDbContext _context = context;
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            return _context.Categorias.ToList();
+        }
+    }
+}
