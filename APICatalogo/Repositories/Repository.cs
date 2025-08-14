bcda4fac05950745e81ffcbd0fc3de9f0e94fe75
tdias <dias.talitaa22@gmail.com>
2025-08-14 14:22:06 -0400

    feat:add concrete class of repository interface

---
+++ APICatalogo/Repositories/Repository.cs
@@ -0,0 +1,43 @@
+using System.Linq.Expressions;
+using APICatalogo.Context;
+
+namespace APICatalogo.Repositories;
+
+public class Repository<T>(AppDbContext context) : IRepository<T> where T : class
+{
+    private readonly AppDbContext _context = context;
+
+    public IEnumerable<T> GetAll()
+    {
+        return _context.Set<T>().ToList();
+    }
+
+    public T? Get(Expression<Func<T, bool>> predicate)
+    {
+        return _context.Set<T>().SingleOrDefault(predicate);
+    }
+
+    public T Create(T entity)
+    {
+        _context.Set<T>().Add(entity);
+        _context.SaveChanges();
+
+        return entity;
+    }
+
+    public T Update(T entity)
+    {
+        _context.Set<T>().Update(entity);
+        _context.SaveChanges();
+
+        return entity;
+    }
+
+    public T Delete(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+        _context.SaveChanges();
+
+        return entity;
+    }
+}
